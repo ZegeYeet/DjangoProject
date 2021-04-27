53f599d5501bdb7d8a661e113d4884b00aea6df5
Zegetable <38813761+Zegetable@users.noreply.github.com>
2021-04-27 12:49:43 -0500

    create new, delete, modify posts added

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
-    path('', views.home, name = "blog-home"), #empty path means it is the default/home
+    path('', PostListView.as_view(), name = "blog-home"), #empty path is default
+    path('post/<int:pk>/', PostDetailView.as_view(), name = "post-detail"), #pk is convention for primary key for detail view
+    path('post/new/', PostCreateView.as_view(), name = "post-create"), 
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = "post-update"), 
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = "post-delete"), 
     path('about/', views.about, name = "blog-about"),
 ]
 
